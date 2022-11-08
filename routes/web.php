d65ebde2f4fe31d503dd971c2f8f673e10feb3a7
Sedat IŞLEK <sdt6@hotmail.com>
2022-11-09 02:21:58 +0300

    Yönetici Paneli

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,25 @@
 <?php
 
 use App\Http\Controllers\Front\Homepage;
+use App\Http\Controllers\Back\Dashboard;
+use App\Http\Controllers\Back\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Back Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+*/
+
+Route::get('admin/panel',[Dashboard::class, 'index'])->name('admin.dashboard');
+
+Route::get('admin/giris',[Auth::class,'login'])->name('admin.login');
+
+/*
+|--------------------------------------------------------------------------
+| Front Routes
+|--------------------------------------------------------------------------
 |
 */
 
