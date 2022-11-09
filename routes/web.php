4574c56ab2354688efe886f0ecb05cb0e11f5088
Sedat IŞLEK <sdt6@hotmail.com>
2022-11-09 11:34:09 +0300

    Admin Auth - Middleware ile güvenlik

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\Front\Homepage;
 use App\Http\Controllers\Back\Dashboard;
-use App\Http\Controllers\Back\Auth;
+use App\Http\Controllers\Back\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('admin/panel',[Dashboard::class, 'index'])->name('admin.dashboard');
 
-Route::get('admin/giris',[Auth::class,'login'])->name('admin.login');
+Route::get('admin/giris',[AuthController::class,'login'])->name('admin.login');
+
+Route::post('admin/giris',[AuthController::class,'loginPost'])->name('admin.login.post');
+
+Route::get('admin/cikis', [AuthController::class,'logout'])->name('admin.logout');
 
 /*
 |--------------------------------------------------------------------------
