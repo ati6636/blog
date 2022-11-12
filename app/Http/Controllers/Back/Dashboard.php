842ebe38918ec2039665265ea38ecdd36ccba49d
Sedat IŞLEK <sdt6@hotmail.com>
2022-11-12 17:43:12 +0300

    Algoritma Düzenlemeleri

--- app/Http/Controllers/Back/Dashboard.php
+++ app/Http/Controllers/Back/Dashboard.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers\Back;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Page;
+use App\Models\Article;
 
 class Dashboard extends Controller
 {
     public function index(){
-      return view('back.dashboard');
+      $article = Article::all()->count();
+      $hit = Article::sum('hit');
+      $category = Category::all()->count();
+      $page = Page::all()->count();
+      return view('back.dashboard',compact('article','hit','category','page'));
     }
 }
